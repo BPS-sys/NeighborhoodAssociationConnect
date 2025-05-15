c1d0fdbf24177ab31be5e43b67a73091dec0c503
MukaihiraJyunta <ok220179@oks.iput.ac.jp>
2025-05-15 15:58:59 +0900

    toolbarほぼ完成

---
+++ dev/frontend/my-expo-app/components/BottomTabNavigator.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const BottomTabNavigator = () => {
+  return (
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          switch (route.name) {
+            case "index":
+              return (
+                <Ionicons
+                  name={focused ? "home" : "home-outline"}
+                  size={size}
+                  color={color}
+                />
+              );
+            case "board":
+              return (
+                <MaterialCommunityIcons
+                  name={focused ? "clipboard-text" : "clipboard-text-outline"}
+                  size={size}
+                  color={color}
+                />
+              );
+            case "chat":
+              return (
+                <Ionicons
+                  name={focused ? "chatbubble" : "chatbubble-outline"}
+                  size={size}
+                  color={color}
+                />
+              );
+            case "schedule":
+              return (
+                <Ionicons
+                  name={focused ? "calendar" : "calendar-outline"}
+                  size={size}
+                  color={color}
+                />
+              );
+          }
+        },
+        tabBarLabelStyle: { fontSize: 12 },
+        tabBarStyle: { height: 60, paddingBottom: 5 },
+        headerShown: false,
+      })}
+    >
+      <Tabs.Screen name="index" options={{ title: "ホーム" }} />
+      <Tabs.Screen name="board" options={{ title: "掲示板" }} />
+      <Tabs.Screen name="chat" options={{ title: "チャット" }} />
+      <Tabs.Screen name="schedule" options={{ title: "スケジュール" }} />
+    </Tabs>
+  );
+};
+
+export default BottomTabNavigator;
