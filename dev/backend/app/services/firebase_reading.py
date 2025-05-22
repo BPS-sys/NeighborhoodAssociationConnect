74714ba388bb80211f86d63931b02f2f13c4598f
BPS-sys <prgrmsilver@gmail.com>
2025-05-22 14:29:26 +0900

    [update].envファイルから読み込むように変更

--- dev/backend/app/services/firebase_reading.py
+++ dev/backend/app/services/firebase_reading.py
@@ -2,6 +2,10 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import json
 from datetime import datetime
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 class FirestoreEncoder(json.JSONEncoder):
     """
@@ -47,7 +51,23 @@ def get_all_news_json(region_id, page_size=100, last_doc=None):
         # Firebase初期化（シングルトンパターン）
         if not firebase_admin._apps:
             cred = credentials.Certificate(
-                r"Firebase_地域共生ID.json"
+                {
+                    "type": os.getenv("FIREBASE_TYPE"),
+                    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+                    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+                    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace(
+                        "\\n", "\n"
+                    ),
+                    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+                    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+                    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+                    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+                    "auth_provider_x509_cert_url": os.getenv(
+                        "FIREBASE_AUTH_PROVIDER_X509_CERT_URL"
+                    ),
+                    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+                    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
+                }
             )
             firebase_admin.initialize_app(cred)
 
